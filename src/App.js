bbb80c90c3dd9f317521f04a779622a3e79d2884
gaowj <gaowj@datacloudchina.com>
2018-03-13 17:25:25 +0800

    remote branch

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
+function Hello(props) {
+	function handleClick(e) {
+	    e.preventDefault();
+	    console.log('The link was clicked')
+	  }
+  return <h1 onClick={handleClick}>Hello, {props.name}</h1>
+}
+
 function App() {
   return (
     <div>
       <Welcome name="gaowj" />
-      <Welcome name="Cahal" />
+      <Welcome name="yanmenglu" />
       <Welcome name="Edite" />
+      <Hello name="James" />
     </div>
   );
 }
